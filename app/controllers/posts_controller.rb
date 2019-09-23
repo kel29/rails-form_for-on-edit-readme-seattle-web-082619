93baf69143b22dd55178ad8e69b984e47ea3549c
Krista Loven <krista.loven.87@gmail.com>
2019-09-23 14:17:30 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 	end
 
 	def show
-		@post = Post.find(params[:id])
+		set_post
 	end
 
 	def new
@@ -12,20 +12,30 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new
-	  @post.title = params[:title]
+		@post = Post.new
+		@post.title = params[:title]
 	  @post.description = params[:description]
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def edit
-		@post = Post.find(params[:id])
+		set_post
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
+		
+	  set_post
+		@post.update(post_params)
 	  redirect_to post_path(@post)
 	end
+
+	def set_post
+		@post = Post.find(params[:id])
+	end
+
+	def post_params
+		params.require(:post).permit(:title, :description)
+	end
+
 end
